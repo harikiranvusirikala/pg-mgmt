104b3aa2c95af866499ebde555c45169ae9a5272
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2025-10-04 14:10:51 +0530

    Update Angular dependencies and add chart libraries
    Upgraded Angular packages to 20.3.3/20.3.4 and updated build budgets in angular.json. Added chart.js and ng2-charts dependencies, removed unused styles, and included a new SVG icon. Updated app.module.ts and index.html to support new features.

--- pg-mgmt-frontend/src/app/app.module.ts
+++ pg-mgmt-frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import {
 } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { provideHotToastConfig } from '@ngxpert/hot-toast';
 // eslint-disable-next-line deprecation/deprecation
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
@@ -18,6 +17,9 @@ import {
 } from '@abacritt/angularx-social-login';
 import { LoginComponent } from './user/login/login.component';
 
+/**
+ * Bootstraps the Angular application with shared modules and feature screens.
+ */
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,40 +29,10 @@ import { LoginComponent } from './user/login/login.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxSpinnerModule,
     // eslint-disable-next-line deprecation/deprecation
     BrowserAnimationsModule,
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
-    provideHotToastConfig({
-      position: 'top-right',
-      duration: 15000,
-      dismissible: true,
-      style: {
-        background: '#333',
-        color: '#fff',
-      },
-      success: {
-        style: { background: '#28a745', color: '#fff' },
-        duration: 15000,
-      },
-      error: {
-        style: { background: '#dc3545', color: '#fff' },
-        duration: 15000,
-      },
-      info: {
-        style: { background: '#17a2b8', color: '#fff' },
-        duration: 15000,
-      },
-      warning: {
-        style: { background: '#ffc107', color: '#212529' },
-        duration: 15000,
-      },
-      loading: {
-        style: { background: '#6c757d', color: '#fff' },
-        duration: 15000,
-      },
-    }),
 })
 export class AppModule {}
